5226d304b10c16eb65eeba7d402c4c05d5c5633e
klustar <klustar@naver.com>
2020-02-26 20:52:02 +0900

    아두이노에서 모스부호를 생성하는 기능만 구현

---
+++ morse/morse.cpp
@@ -0,0 +1,777 @@
+#include "Arduino.h"
+#include "morse.h"
+
+morse::morse(int pin) {
+	pinMode(pin, OUTPUT);
+	_pin = pin;
+}
+
+void morse::signal_A(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_B(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_C(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_D(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_E(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_F(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_G(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_H(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_I(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_J(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_K(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_L(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_M(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_N(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_O(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_P(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_Q(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_R(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_S(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_T(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_U(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_V(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_W(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_X(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_Y(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_Z(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+
+//아라비아숫자 메소드
+void morse::signal_1(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_2(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_3(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_4(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_5(){
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_6(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_7(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_8(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_9(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //짧은 신호
+    digitalWrite(_pin, HIGH);
+    delay(500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
+
+void morse::signal_0(){
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+    //긴 신호
+    digitalWrite(_pin, HIGH);
+    delay(1500);
+    digitalWrite(_pin, LOW);
+    delay(500);
+}
---
+++ morse/morse.h
@@ -0,0 +1,54 @@
+#ifndef morse_h
+#define morse_h
+
+#include "Arduino.h"
+
+class morse {
+public:
+	morse(int pin);
+
+    //영문자 메소드
+	void signal_A();
+    void signal_B();
+    void signal_C();
+    void signal_D();
+    void signal_E();
+    void signal_F();
+    void signal_G();
+    void signal_H();
+    void signal_I();
+    void signal_J();
+    void signal_K();
+    void signal_L();
+    void signal_M();
+    void signal_N();
+    void signal_O();
+    void signal_P();
+    void signal_Q();
+    void signal_R();
+    void signal_S();
+    void signal_T();
+    void signal_U();
+    void signal_V();
+    void signal_W();
+    void signal_X();
+    void signal_Y();
+    void signal_Z();
+
+    //아라비아숫자 메소드
+    void signal_1();
+    void signal_2();
+    void signal_3();
+    void signal_4();
+    void signal_5();
+    void signal_6();
+    void signal_7();
+    void signal_8();
+    void signal_9();
+    void signal_0();
+    
+private:
+	int _pin;
+};
+
+#endif
