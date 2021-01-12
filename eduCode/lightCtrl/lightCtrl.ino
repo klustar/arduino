154c5c843421000c785ff87b71c947d3dd4b419a
klustar <klustar@naver.com>
2021-01-12 16:58:24 +0900

    식물성장용 조명제어

---
+++ eduCode/lightCtrl/lightCtrl.ino
@@ -0,0 +1,30 @@
+#define toggle 4    // 전원스위치
+#define cluster 5   // 식물조명제어
+
+boolean state = 0;
+
+void setup() {
+  pinMode(cluster, OUTPUT);
+  pinMode(toggle, INPUT);
+}
+
+void loop() {
+  if (digitalRead(toggle) == 1) {
+    if (state == true) {
+      state == false;
+    }
+    else {
+      state == true;
+    }
+  }
+
+  if (state == true) {
+    digitalWrite(cluster, LOW);
+    delay(50);
+    digitalWrite(cluster, HIGH);
+    delay(150);
+  }
+  else {
+    digitalWrite(cluster, LOW);
+  }
+}
