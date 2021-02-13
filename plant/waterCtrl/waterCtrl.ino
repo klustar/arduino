c4a1255d1ea75fab16434921f5f63d757b76ba7c
klustar <klustar@naver.com>
2021-02-13 17:36:28 +0900

    주석 추가

--- plant/waterCtrl/waterCtrl.ino
+++ plant/waterCtrl/waterCtrl.ino
@@ -17,8 +17,9 @@ void setup() {
 void loop() {
   level = analogRead(waterIN);
   level = map(level, 0, 1023, 0, 100); // 수분도 퍼센트 변환
-  Serial.print(level);
-  Serial.print("   ");
+  
+  //현재는 시리얼모니터에 출력하지만,
+  //추후 현재시간과 함께 데이터베이스에 저장
   Serial.println(level);
 
   if (level < wet) {
@@ -26,12 +27,14 @@ void loop() {
     digitalWrite(green, LOW);
     digitalWrite(red, HIGH);
     digitalWrite(waterOUT, HIGH);
+    delay(10);
   }
   else {
     // 물을 주지않아야하는 상황
     digitalWrite(green, HIGH);
     digitalWrite(red, LOW);
     digitalWrite(waterOUT, LOW);
+    delay(10);
   }
   delay(10);
 }
